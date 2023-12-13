7dc63dfb0ea6e8e9dcb7d083e5b0eaef1b597669
SumbitScreen2027 <149934679+SumbitScreen2027@users.noreply.github.com>
2023-12-13 16:47:18 +0000

    edit

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const clock = () => {
-  console.log("you have been #@€_& up by "memesScreeny" bruh")
+  console.log("you have been #@€_& up by memesScreeny bruh")
   console.log("get virus!!!!!")
 };
 console.log(clock)
